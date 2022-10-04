2939bb6514e90d7f7578c6ab813b5beaa7b5cc88
Anna <polisuk092@gmail.com>
2022-10-05 00:31:12 +0300

    feat: create burger menu

--- online-zoo/pages/donate/script.js
+++ online-zoo/pages/donate/script.js
@@ -23,3 +23,22 @@ function limitOfInput(element) {
     }
 }
 
+
+
+// Бургер меню
+const burgerMenu = document.querySelector('.menu-burger');
+const menu = document.querySelector('.nav__list');
+const logo = document.querySelector('.nav__logo_640');
+const shadow = document.querySelector('.shadow');
+const designedBy = document.querySelector('.nav__designedby');
+
+function openAndCloseMenu() {
+    menu.classList.toggle('nav__list_active');
+    burgerMenu.classList.toggle('menu-burger_active');
+    logo.classList.toggle('nav__logo_active');
+    shadow.classList.toggle('shadow_active');
+    designedBy.classList.toggle('nav__designedby_active');
+}
+
+burgerMenu.addEventListener('click', openAndCloseMenu);
+shadow.addEventListener('click', openAndCloseMenu);
--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -1,13 +1,12 @@
 const navLinks = document.querySelectorAll('.menu__link');
 const footerLinks = document.querySelectorAll('.footer-menu__link');
 
+// Подсвечивание активной ссылки меню
 navLinks.forEach(link => {
     if (link.getAttribute('href') === window.location.pathname) {
         link.classList.add('menu__link_active');
     }
 })
-
-
 
 
 footerLinks.forEach(link => {
@@ -17,3 +16,20 @@ footerLinks.forEach(link => {
 })
 
 
+// Бургер меню
+const burgerMenu = document.querySelector('.menu-burger');
+const menu = document.querySelector('.nav__list');
+const logo = document.querySelector('.nav__logo_640');
+const shadow = document.querySelector('.shadow');
+const designedBy = document.querySelector('.nav__designedby');
+
+function openAndCloseMenu() {
+    menu.classList.toggle('nav__list_active');
+    burgerMenu.classList.toggle('menu-burger_active');
+    logo.classList.toggle('nav__logo_active');
+    shadow.classList.toggle('shadow_active');
+    designedBy.classList.toggle('nav__designedby_active');
+}
+
+burgerMenu.addEventListener('click', openAndCloseMenu);
+shadow.addEventListener('click', openAndCloseMenu);
