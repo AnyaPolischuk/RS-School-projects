6a2d58b731c3dc06a9ed67b40cebe57bd4f5a018
Anna <polisuk092@gmail.com>
2022-12-11 22:30:20 +0300

    feat: stylize news card

--- news-JS/src/components/controller/appLoader.ts
+++ news-JS/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: '36f7599ad4ca4382b47aa75a0dc638d5', // получите свой ключ https://newsapi.org/
         });
     }
