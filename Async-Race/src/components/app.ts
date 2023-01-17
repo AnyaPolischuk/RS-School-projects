4714dd895ddeae8bb8e28e635a969482edcb7487
Anna <polisuk092@gmail.com>
2023-01-17 18:36:34 +0300

    feat: add fetch requests and create btns and input

---
+++ Async-Race/src/components/app.ts
@@ -0,0 +1,95 @@
+interface IVariables {
+    GARAGE: string;
+    ENGINE: string;
+    WINNERS: string;
+}
+
+interface IBody {
+    name: string;
+    color: string;
+}
+
+const variables: IVariables = {
+    GARAGE: 'http://127.0.0.1:3000/garage',
+    ENGINE: 'http://127.0.0.1:3000/engine',
+    WINNERS: 'http://127.0.0.1:3000/winners',
+};
+
+export const getCars = async (page?: number, limit = 7) => {
+    const response = await fetch(`${variables.GARAGE}?_page=${page}&_limit=${limit}`);
+
+    return {
+        cars: await response.json(),
+        amount: response.headers.get('X-Total-Count'),
+    };
+};
+
+export const getCar = async (id: number) => {
+    const response = await fetch(`${variables.GARAGE}/${id}`);
+    return await response.json();
+};
+
+export const createCar = async (body: IBody) => {
+    const response = await fetch(`${variables.GARAGE}`, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' },
+    });
+    return await response.json();
+};
+
+export const deleteCar = async (id: number) => {
+    const response = await fetch(`${variables.GARAGE}/${id}`, {
+        method: 'DELETE',
+    });
+    return await response.json();
+};
+
+export const updateCar = async (id: number, body: IBody) => {
+    const response = await fetch(`${variables.GARAGE}/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json' },
+    });
+    return await response.json();
+};
+
+export const startCarEngine = async (id: number) => {
+    const response = await fetch(`${variables.ENGINE}?id=${id}&status=started`, {
+        method: 'PATCH',
+    });
+    return await response.json();
+};
+
+export const stopCarEngine = async (id: number) => {
+    const response = await fetch(`${variables.ENGINE}?id=${id}&status=stopped`, {
+        method: 'PATCH',
+    });
+    return await response.json();
+};
+
+export const switchCarsEngine = async (id: number) => {
+    const response = await fetch(`${variables.ENGINE}?id=${id}&status=drive`, {
+        method: 'PATCH',
+    });
+    if (response.status === 200) {
+        return await response.json();
+    } else if (response.status === 404) {
+        return 'Engine parameters for car with such id was not found in the garage. Have you tried to set engine status to "started" before?';
+    } else if (response.status === 429) {
+        return 'Drive already in progress. You can not run drive for the same car twice while it is not stopped.';
+    } else if (response.status === 500) {
+        return 'Car has been stopped suddenly. It is engine was broken down.';
+    }
+};
+
+// export const getWinners = async (
+//     page?: number,
+//     limit?: number,
+//     sort?: 'id' | 'wins' | 'time',
+//     order?: 'ASC' | 'DESC'
+// ) => {
+//     if (limit) {
+//       const response = await fetch(`${variables.WINNERS}`)
+//     }
+// };
---
+++ Async-Race/src/index.ts
@@ -0,0 +1,104 @@
+import { renderGarage, renderWinners, showInfoAboutGarage } from './components/garage';
+import { getCars, getCar, createCar, deleteCar, updateCar, startCarEngine, stopCarEngine, switchCarsEngine } from './components/app';
+
+// getCars(1)
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// getCar(2)
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// createCar({
+//     name: 'New Red Car',
+//     color: '#ff0000',
+// })
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// getCars(1)
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// deleteCar(3).then((res) => {
+//     console.log(res);
+// });
+
+// getCars(1)
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// updateCar(1, {
+//     name: 'KEK',
+//     color: '#ff0000',
+// })
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// getCars(1)
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+// startCarEngine(4)
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .then((err) => {
+//         console.log(err);
+//     });
+
+// stopCarEngine(4)
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .then((err) => {
+//         console.log(err);
+//     });
+
+// startCarEngine(4)
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .then((err) => {
+//         console.log(err);
+//     });
+
+// switchCarsEngine(4)
+//     .then((res) => {
+//         console.log(res);
+//     })
+//     .then((err) => {
+//         console.log(err);
+//     });
+
+renderGarage();
+// renderWinners();
+showInfoAboutGarage();
